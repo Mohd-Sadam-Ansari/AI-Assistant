a2d63393b2567a9b6169dd2a6d03babdb6bfd295
Mohd-Sadam-Ansari <mohdsadamansari21-23@bhavans.ac.in>
2023-04-01 15:19:05 +0530

    update browser functions

--- features/browser.py
+++ features/browser.py
@@ -5,19 +5,52 @@ from pywikihow import search_wikihow
 import wikipedia
 
 def play_on_youtube(query):
+    query=query.replace('alan','')
+    query=query.replace('ellen','')
+    query=query.replace('play song on youtube','')
+    query=query.replace('youtube','')
+    query=query.replace('video','')
+    query=query.replace('movie','')
+    query=query.replace('search','')
+
     pywhatkit.playonyt(query)
-    pyautogui.keyDown('enter')
-    pyautogui.keyUp('enter') 
+    pyautogui.press('enter')
     return True  
 
 def sites(data):
     webbrowser.open(f'www.{data}.com')
     return True
 
+def online_order(query):
+    query=query.replace("find me",'')
+    query=query.replace("i want to order a",'')
+    query=query.replace("i want to order",'')
+    query=query.replace("order a",'')
+    query=query.replace("order me a",'')
+    query=query.replace('alan','')
+    query=query.replace('ellen','')
+
+    if 'pizza' in query:
+        web="https://pizzaonline.dominos.co.in/menu?"
+        webbrowser.open(web)
+    elif 'food' in query:
+        web='https://www.zomato.com/mumbai/order-food-online'
+        webbrowser.open(web)
+    else:
+        web='https://www.flipkart.com/search?q='+query
+        webbrowser.open(web)
+
 def google_search(query):
-    pywhatkit.search(query)
-    wikipedia.summary(query,2)
-    return True
+    query=query.replace('alan','')
+    query=query.replace('ellen','')
+    query=query.replace('google search','')
+    query=query.replace('google','')
+    try:
+        pywhatkit.search(query)
+        result=wikipedia.summary(query,2)
+        return result
+    except:
+        return "No speakable content available."
 
 def how_to(query):
     max_results=1
